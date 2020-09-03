669fc3443d0e92a5fbe7c325691a10b3455e3583
shubhamhr07 <shubhamhr07@github.com>
2020-09-03 06:05:02 +0530

    new game

---
+++ game.py
@@ -0,0 +1,18 @@
+#https://github.com/shubhamhr07/tictactoe.git
+
+def head():
+    print("tic    1|2|3  ")
+    print("tac    4|5|6  ")
+    print("toe    7|8|9  ")
+
+a=["","","","","","","","","",""]
+
+def game_board():
+    print(a[1]+" | "+a[2]+" | "+a[3])
+    print("")
+    print(a[4]+" | "+a[5]+" | "+a[6])
+
+    print(a[7]+" | "+a[8]+" | "+a[9])
+
+game_board()
+
