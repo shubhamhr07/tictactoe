8e3321feafbf2f6bc82ed0f8ef9e22579491ef9f
shubhamhr07 <shubhamhr07@github.com>
2020-09-07 00:54:49 +0530

    DONE FOR NOW

--- game.py
+++ game.py
@@ -1,7 +1,8 @@
 #https://github.com/shubhamhr07/tictactoe.git
+import os
 
 def head():
-    print("tic    1|2|3  ")
+    print("tic    1|2|3    Enter 0 to exit ")
     print("tac    4|5|6  ")
     print("toe    7|8|9  ")
 
@@ -14,48 +15,55 @@ def game_board():
     print("---------")
     print(a[7]+" | "+a[8]+" | "+a[9])
 
+def win_msg():
+    game_board()
+    print("\t~~~~  "+turn+" WON  ~~~~")
+    exit()
+
 def winner():
-    if a[1]==a[2]==a[3]!=" ":
-        print(a[1] +"is winner")
-        next = False
-    elif a[4]==a[5]==a[6]!=" ":
-        print(a[4] +"is winner")
-        next = False
-    elif a[7]==a[8]==a[9]!=" ":
-        print(a[7] +"is winner")
-        next = False
-    elif a[1]==a[4]==a[7]!=" ":
-        print(a[1] +"is winner")
-        next = False
-    elif a[2]==a[5]==a[8]!=" ":
-        print(a[2] +"is winner")
-        next = False
-    elif a[3]==a[6]==a[9]!=" ":
-        print(a[3] +"is winner")
-        next = False
-    elif a[1]==a[5]==a[9]!=" ":
-        print(a[1] +"is winner")
-        next = False
-    elif a[3]==a[5]==a[7]!=" ":
-        print(a[7] +"is winner")
-        next = False
+    if (a[1]==a[2]==a[3]!=" "):
+        win_msg()
+    elif (a[4]==a[5]==a[6]!=" "):
+        win_msg()
+    elif (a[7]==a[8]==a[9]!=" "):
+        win_msg()
+    elif (a[1]==a[4]==a[7]!=" "):
+        win_msg()
+    elif (a[2]==a[5]==a[8]!=" "):
+        win_msg()
+    elif (a[3]==a[6]==a[9]!=" "):
+        win_msg()
+    elif (a[1]==a[5]==a[9]!=" "):
+        win_msg()
+    elif (a[3]==a[5]==a[7]!=" "):
+        win_msg()
     else:
-        next = True
+        os.system('clear')
+        
+def p_input():
+    move=int(input("Enter the position for "+turn+" \n\t"))
+    if move==0:
+        print("\t~~~~~~GAME OVER~~~~~~")
+        exit()
+    if a[move]==" ":
+        a[move]=turn
+    else:
+        print("Enter the empty position \n\t")
+        p_input()
+        
+turn="0"
 
-head()
-game_board()
+for i in range(10):
+    head()
+    game_board()
+    p_input()
+    winner()
+    if turn == "0":
+        turn="X"
+    else:
+        turn ="0"
     
-x=int(input("Enter the position for '0'\n\t"))
-a[x]="0"
-    
-winner()
-
-head()
-game_board()
-    
-y=int(input("Enter the position for 'x'\n\t"))
-a[y]="x"
-    
-winner()   
-
-print("Game Over")
+    if i == 8:
+        game_board()
+        print("DRAW")
+        exit()
